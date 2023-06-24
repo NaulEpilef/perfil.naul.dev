df8b356057f8897dcdf55ba8d42e3209a4aee37d
Luan Felipe <lfps1999@gmail.com>
2023-06-24 14:16:28 -0300

    Creating the basic structure

---
+++ src/app/gerador/senha/page.tsx
@@ -0,0 +1,11 @@
+import Main from "@/components/Main";
+
+const password = () => {
+    return (
+        <Main>
+            <h1>Gerador de senha</h1>
+        </Main>
+    );
+}
+
+export default password;
